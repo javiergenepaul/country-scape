fc6583d44bcedad2d3bf1561b8acbcc2ec535736
javiergenepaul <javiergenepaul@gmail.com>
2022-11-24 11:18:40 +0800

    update button blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -15,8 +15,10 @@ function Blog() {
 
 	const [scrollValue, setScrollValue] = useState(0);
 	const [scrollEnd, setScrollEnd] = useState(0);
-	const [prevBtnColor, setprevBtnColor] = useState('secondary-5');
-	const [nextBtnColor, setNexBtnColor] = useState('primary-1');
+	const [prevBtnStrokeColor, setprevBtnStrokeColor] = useState('stroke-secondary-5');
+	const [nextBtnStrokeColor, setNexBtnStrokeColor] = useState('stroke-primary-1');
+	const [prevBtnBorderColor, setprevBtnBorderColor] = useState('border-secondary-5');
+	const [nextBtnBorderColor, setNexBtnBorderColor] = useState('border-primary-1');
 
 	useEffect(() => {
 		const onScroll = (e) => {
@@ -24,12 +26,17 @@ function Blog() {
 		};
 
 		if (scrollValue == 0) {
-			setprevBtnColor('secondary-5')
+			setprevBtnStrokeColor('stroke-secondary-5');
+			setprevBtnBorderColor('border-secondary-5');
 		} else if(scrollValue == scrollEnd) {
-			setNexBtnColor('secondary-5');
+			setNexBtnStrokeColor('stroke-secondary-5');
+			setNexBtnBorderColor('border-secondary-5');
 		} else {
-			setNexBtnColor('primary-1');
-			setprevBtnColor('primary-1')
+			setNexBtnStrokeColor('stroke-primary-1');
+			setprevBtnStrokeColor('stroke-primary-1');
+
+			setNexBtnBorderColor('border-primary-1');
+			setprevBtnBorderColor('border-primary-1');
 		}
 
 		document.getElementById('slider').addEventListener('scroll', onScroll);
@@ -43,13 +50,14 @@ function Blog() {
 			<div className="flex justify-between">
 				<span className='text-h3 font-medium'>latest Blog</span>
 				<div className="flex gap-2">
-					<button className={`p-2 border rounded-lg border-${prevBtnColor}`} onClick={slideLeft}>
-						<svg className={`stroke-2 rounded stroke-${prevBtnColor}`} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					
+					<button className={`p-2 border rounded-lg ${prevBtnBorderColor}`} onClick={slideLeft}>
+						<svg className={`stroke-2 rounded ${prevBtnStrokeColor}`} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M7 16L3 12M3 12L7 8M3 12H21" />
 						</svg>
 					</button>
-					<button className={`p-2 border rounded-lg border-${nextBtnColor}`} onClick={slideRight}>
-						<svg className={`stroke-2 rounded stroke-${nextBtnColor}`} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<button className={`p-2 border rounded-lg ${nextBtnBorderColor}`} onClick={slideRight}>
+						<svg className={`stroke-2 rounded ${nextBtnStrokeColor}`} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M17 8L21 12M21 12L17 16M21 12H3" />
 						</svg>
 					</button>
